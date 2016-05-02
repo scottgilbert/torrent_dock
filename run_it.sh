3465898a8bf984317192b4c430029df16db77d9e
Scott Gilbert <scott.gilbert@bum.org>
2016-05-02 20:42:42 +0000

    Corrected path to volume in runit.sh

--- run_it.sh
+++ run_it.sh
@@ -5,5 +5,5 @@
 docker run -d \
            -p 127.0.0.1:9091:9091 \
            -p 0.0.0.0:64314:64314 \
-           -v data:/var/lib/transmission-daemon/downloads \
+           -v ~/torrent_dock/data:/var/lib/transmission-daemon/downloads \
            torrent_dock
