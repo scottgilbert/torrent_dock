878e4d6ba1404ce386e0ac822764de5ab4e07345
Scott Gilbert <scott.gilbert@bum.org>
2016-05-02 21:43:55 +0000

    Added step to fix permissions on data dir to run_it.sh

--- run_it.sh
+++ run_it.sh
@@ -2,6 +2,9 @@
 # uses a container image tagged "torrent_doc" as might be created with:
 # docker build -t torrent_dock
 
+#ensure data dir is writable by container
+chmod 777 ~/torrent_dock/data
+
 docker run -d \
            -p 127.0.0.1:9091:9091 \
            -p 0.0.0.0:64314:64314 \
