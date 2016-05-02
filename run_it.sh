1c2a2e879eba51e860f79c2f1ffc59b78bf0892e
Scott Gilbert <scott.gilbert@bum.org>
2016-05-02 19:59:13 +0000

    Added volume mount and reformatted run_it.sh

--- run_it.sh
+++ run_it.sh
@@ -2,4 +2,8 @@
 # uses a container image tagged "torrent_doc" as might be created with:
 # docker build -t torrent_dock
 
-docker run -d -p 127.0.0.1:9091:9091 -p 0.0.0.0:64314:64314  torrent_dock
+docker run -d \
+           -p 127.0.0.1:9091:9091 \
+           -p 0.0.0.0:64314:64314 \
+           -v data:/var/lib/transmission-daemon/downloads \
+           torrent_dock
