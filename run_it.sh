c7665f2f4c73e29cbc03201988afe333f2c02e87
Scott Gilbert <scott.gilbert@bum.org>
2016-05-04 21:03:18 +0000

    Some fixes to systemd startup

--- run_it.sh
+++ run_it.sh
@@ -1,21 +1,16 @@
-#/bin/bash
+#/bin/bash -
 # uses a container image tagged "torrent_doc" as might be created with:
 # docker build -t torrent_dock
 
 #ensure data dir is writable by container
-chmod 777 ~/torrent_dock/data
+chmod 777 /root/torrent_dock/data
 
-if [[ "$1" != "" ]]; then
-   options="$1"
-else
-   options='-d'
-fi
-
-docker run $options \
+/usr/bin/docker run  \
            -p 127.0.0.1:9091:9091 \
            -p 0.0.0.0:64314:64314 \
-           -v ~/torrent_dock/data:/var/lib/transmission-daemon/downloads \
-           -v ~/torrent_dock/torrents:/var/lib/transmission-daemon/.config/transmission-daemon/torrents:ro \
+           -v /root/torrent_dock/data:/var/lib/transmission-daemon/downloads \
+           -v /root/torrent_dock/torrents:/var/lib/transmission-daemon/.config/transmission-daemon/torrents:ro \
+           --rm=true \
            --name="torrent_dock" \
            torrent_dock
 
