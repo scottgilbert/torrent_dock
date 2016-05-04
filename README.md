1e3ff141f556e000d6f62e8566f44c59bf9e7622
Scott Gilbert <scott.gilbert@bum.org>
2016-05-04 16:34:01 +0000

    Initial work to provide systemd auto-start of container at boot

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ On a new host, one should be able to run this container following these steps:
  1. ./run_it.sh
 
 Updating the container should just be a matter of re-running built_it.sh, stopping any existing container, followed by run_it.sh.
+
+(The following does not seem to be working yet.  A work in progress....)
+To make container auto-start at boot time:
+ 1. Copy torrent_dock.service to /etc/systemd/system/
+ 1. systemctl daemon-reload
+ 1. systemctl enable torrent_dock
+
+Note: Due to sloppiness and laziness, there are multiple assumptions that this content is in the /root/torrent_dock directory.
--- run_it.sh
+++ run_it.sh
@@ -5,10 +5,17 @@
 #ensure data dir is writable by container
 chmod 777 ~/torrent_dock/data
 
-docker run -d \
+if [[ "$1" != "" ]]; then
+   options="$1"
+else
+   options='-d'
+fi
+
+docker run $options \
            -p 127.0.0.1:9091:9091 \
            -p 0.0.0.0:64314:64314 \
            -v ~/torrent_dock/data:/var/lib/transmission-daemon/downloads \
-           -v ~/torrent_dock/torrents:/var/lib/transmission-daemon/.config/transmission-daemon/torrents \
+           -v ~/torrent_dock/torrents:/var/lib/transmission-daemon/.config/transmission-daemon/torrents:ro \
+           --name="torrent_dock" \
            torrent_dock
 
